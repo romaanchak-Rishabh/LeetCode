da54c3b37908439eddc22b9bf8b19a7357470ea5
Rishabh Tater <rishabhtater142@gmail.com>
2025-05-04 02:52:02 +0530

    Time: 0 ms (100%), Space: 21.6 MB (92.3%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,9 +1,27 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int ans = 0;
+
+        // Sum difference Approach
+        // TC: O(N) SC: O(1)
+        // int total=(nums.size()*(nums.size()+1))/2;
+        // int missing_total = 0;
+        // for(int i=0; i<nums.size(); i++) {
+        //     missing_total+=nums[i];
+        // }
+        // return total-missing_total;
+
+        //XOR Approach
+        // TC: O(N) SC: O(1)
+        int ans=0;
+        // 1: XOR all values in array
         for(int i=0; i<nums.size(); i++) {
-            ans = i+1 - nums[i] + ans;
+            ans^=nums[i];
+        }
+
+        // 2: XOR all the values in [0,N]
+        for(int i=0; i<=nums.size(); i++) {
+            ans^=i;
         }
         return ans;
     }
