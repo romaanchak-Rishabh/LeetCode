1f7ed19c0aab55b3da68ecc81bac199b9b0841a5
Rishabh Tater <rishabhtater142@gmail.com>
2025-05-10 17:22:24 +0530

    Time: 0 ms (100%), Space: 11.8 MB (41.13%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int index=0;
-        for(int i=0; i<nums.size(); i++) if(nums[i]!=val) nums[index++]=nums[i];
+        // Replace each number with a valid number and then moving forward
+        // TC: O(n) SC: O(1)
+        int index = 0;
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i]!=val) nums[index++] = nums[i];
+        }
         return index;
     }
 };
