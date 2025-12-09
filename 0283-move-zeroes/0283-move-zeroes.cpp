fa7f0b344a99192e06608eec93e2befae6bfb349
Rishabh Tater <rishabhtater142@gmail.com>
2025-12-09 23:41:50 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-
-        // Two pointer approach
-        // TC: O(n) SC: O(1)
-        int i=0;
-        int j=0;
-
-        while(j<nums.size()) {
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        while(j<n) {
             if(nums[j]!=0) swap(nums[i++], nums[j++]);
             else j++;
         }
