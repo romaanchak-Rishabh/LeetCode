d0bb179deace815e46325cd14358abd114955233
Rishabh Tater <rishabhtater142@gmail.com>
2026-08-02 02:13:14 +0530

    Time: 47 ms (29.03%), Space: 53.1 MB (79.52%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return () => "Hello World"
+    return function(...args) {
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
